db0a1f14ad22f46e2718825fc99a9580ea4a6188
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-10 09:43:53 -0300

    Final message problem

--- script.js
+++ script.js
@@ -29,24 +29,24 @@ function pricenumber(price_string){
 
 function select_item() {
 
-    if (price_maindish !== undefined){
-        if (price_drink !== undefined){
-            if (price_dessert !== undefined){
+            if (price_maindish !== undefined && price_drink !== undefined && price_dessert !== undefined){
 
                 const buttom_order = document.querySelector(".checkout a")
                 buttom_order.classList.add("active");
                 buttom_order.innerHTML = "Fechar pedido"
 
                 const total = price_maindish + price_drink + price_drink;
-                order_text = `Olá, gostaria de fazer o pedido: \nPrato: ${type_maindish} \nBebida: ${type_drink} \nSobremesa: ${type_dessert} \nTotal: R$ ${(total).toFixed(2)}`;
+                order_text = `Olá, gostaria de fazer o pedido: \n- Prato: ${type_maindish} \n- Bebida: ${type_drink} \n- Sobremesa: ${type_dessert} \nTotal: R$ ${(total).toFixed(2)} `;
+                
 
                 console.log(`Total: ${total.toFixed(2)}`);
                 console.log(order_text);
+                
+                console.log(type_maindish,type_drink,type_dessert);
         
             }
         }
-    }
-}
+
 
 /*========================
     ROWS
@@ -109,7 +109,6 @@ function select_dessert(part){
 
 }
 
-
 /*========================
     CHECKOUT
 ========================*/
@@ -117,21 +116,30 @@ function select_dessert(part){
 
 function finish_order() {
 
+    const whatsapp = `https://wa.me/5599999999999?text=${encodeURIComponent(order_text)}`;
+    
+    window.open(whatsapp);
 
-
-}
-
-
-function send_order() {
-
-
+    console.log(encodeURIComponent(order_text));
+    /* https://wa.me/5521963120000?text=urldamensagempronta */
 
 }
 
 
-function cancel() {
+/*================================================
+- Peça o nome e endereço
+    
+    Ao clicar em finalizar pedido, lance dois `prompt` para o usuário solicitando seu nome e endereço. 
+    Essas informações devem então serem adicionadas na mensagem final que é enviada por WhatsApp nesse **formato** 
+    (igualzinho, sem nenhum caracter a mais):
 
+    Olá, gostaria de fazer o pedido:
+    - Prato: Frango Yin Yang
+    - Bebida: Coquinha Gelada
+    - Sobremesa: Pudim
+    Total: R$ 27.70
 
+    Nome: Fulano
+    Endereço: Rua...
 
-
-}
+================================================*/
