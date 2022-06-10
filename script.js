d836a7699809ff9a7603d497130fc6e1ca86600d
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-09 21:55:16 -0300

    Added some js to the bottom order, including the flag for complete and incomplete order

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let remove_hidden = document.querySelector(".open_hidden");
+remove_hidden.classList.remove("open_hidden");
+
+let flag_order = false;
+
+if (flag_order == true) {
+    remove_hidden.classList.add("open_hidden");
+    remove_hidden = document.querySelector(".closed_hidden");
+    remove_hidden.classList.remove("closed_hidden");
+
+}
