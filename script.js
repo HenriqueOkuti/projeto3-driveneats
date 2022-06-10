f369404b454b9666ee8603cb50c7880e6d8b6902
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-10 07:18:31 -0300

    Checkout bar repair to be done, js mostly done?

--- script.js
+++ script.js
@@ -2,10 +2,97 @@
     GLOBAL VARIABLES
 ========================*/
 
+let price_maindish = 0;
+let price_drink = 0;
+let price_dessert = 0;
+let total = 0;
+
+let type_maindish = "";
+let type_drink = "";
+let price_dessert = "";
 
 /*========================
-    CARDS
+    AUXILIAR FUNCTIONS
 ========================*/
+
+function pricenumber(price_string){
+
+    price_string = price_string.replace("R$ " , "");
+    price_string = price_string.replace("," , ".");
+    
+    return Number(price_string);
+
+}
+
+
+function select_item() {
+
+    if ((price_maindish !== undefined) && (price_drink !== undefined) && (price_dessert !== undefined)) {
+
+        const buttom_order = document.querySelector(".checkout a")
+        buttom_order.classList.add("active");
+        
+
+    }
+
+
+
+}
+
+/*========================
+    ROWS
+========================*/
+
+function select_maindish(part){
+    const selected_item = document.querySelector(".maindish .selected");
+
+    if (selected_item !== null) {
+        selected_item.classList.remove("selected")
+    }
+
+    part.classList.add("selected");
+
+    let price_string = part.querySelector(".item_value").innerHTML;
+    price_maindish = pricenumber(price_string);
+    type_maindish = part.querySelector().innerHTML;
+
+    select_item();
+
+}
+
+function select_drink(part){
+    const selected_item = document.querySelector(".drink .selected");
+
+    if (selected_item !== null) {
+        selected_item.classList.remove("selected")
+    }
+
+    part.classList.add("selected");
+
+    let price_string = part.querySelector(".item_value").innerHTML;
+    price_drink = pricenumber(price_string);
+    type_drink = part.querySelector().innerHTML;
+
+    select_item();
+
+}
+
+function select_dessert(part){
+    const selected_item = document.querySelector(".dessert .selected");
+
+    if (selected_item !== null) {
+        selected_item.classList.remove("selected")
+    }
+
+    part.classList.add("selected");
+
+    let price_string = part.querySelector(".item_value").innerHTML;
+    price_dessert = pricenumber(price_string);
+    type_dessert = part.querySelector().innerHTML;
+
+    select_item();
+
+}
 
 
 
@@ -14,3 +101,23 @@
 ========================*/
 
 
+function finish_order() {
+
+
+
+}
+
+
+function send_order() {
+
+
+
+}
+
+
+function cancel() {
+
+
+
+
+}
