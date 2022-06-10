a2af718a40d7a368922e33dacc784a9ca59c4208
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-10 08:30:34 -0300

    Missing order message

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
     GLOBAL VARIABLES
 ========================*/
 
-let price_maindish = 0;
-let price_drink = 0;
-let price_dessert = 0;
+let price_maindish = undefined;
+let price_drink = undefined;
+let price_dessert = undefined;
 let total = 0;
 
 let type_maindish = "";
 let type_drink = "";
-let price_dessert = "";
+let type_dessert = "";
 
 /*========================
     AUXILIAR FUNCTIONS
@@ -27,16 +27,26 @@ function pricenumber(price_string){
 
 function select_item() {
 
-    if ((price_maindish !== undefined) && (price_drink !== undefined) && (price_dessert !== undefined)) {
+    if (price_maindish !== undefined){
+        if (price_drink !== undefined){
+            if (price_dessert !== undefined){
 
-        const buttom_order = document.querySelector(".checkout a")
-        buttom_order.classList.add("active");
+                const buttom_order = document.querySelector(".checkout a")
+                buttom_order.classList.add("active");
+                buttom_order.innerHTML = "Fechar pedido"
+
+                const total = price_maindish + price_drink + price_drink;
+                const order_text = `Olá, gostaria de fazer o pedido: \n
+                Prato: ${type_maindish} \n
+                Bebida: ${type_drink} \n
+                Sobremesa: ${type_dessert} \n
+                Total: R$ ${(total).toFixed(2)}`;
+
+                console.log(`Total: ${total.toFixed(2)}`);
         
-
+            }
+        }
     }
-
-
-
 }
 
 /*========================
@@ -47,14 +57,16 @@ function select_maindish(part){
     const selected_item = document.querySelector(".maindish .selected");
 
     if (selected_item !== null) {
-        selected_item.classList.remove("selected")
+        selected_item.classList.remove("selected");
     }
 
     part.classList.add("selected");
 
     let price_string = part.querySelector(".item_value").innerHTML;
     price_maindish = pricenumber(price_string);
-    type_maindish = part.querySelector().innerHTML;
+    type_maindish = part.querySelector(".item_title").innerHTML;
+
+    console.log(price_maindish);
 
     select_item();
 
@@ -64,14 +76,16 @@ function select_drink(part){
     const selected_item = document.querySelector(".drink .selected");
 
     if (selected_item !== null) {
-        selected_item.classList.remove("selected")
+        selected_item.classList.remove("selected");
     }
 
     part.classList.add("selected");
 
     let price_string = part.querySelector(".item_value").innerHTML;
     price_drink = pricenumber(price_string);
-    type_drink = part.querySelector().innerHTML;
+    type_drink = part.querySelector(".item_title").innerHTML;
+
+    console.log(price_drink);
 
     select_item();
 
@@ -81,19 +95,20 @@ function select_dessert(part){
     const selected_item = document.querySelector(".dessert .selected");
 
     if (selected_item !== null) {
-        selected_item.classList.remove("selected")
+        selected_item.classList.remove("selected");
     }
 
     part.classList.add("selected");
 
     let price_string = part.querySelector(".item_value").innerHTML;
     price_dessert = pricenumber(price_string);
-    type_dessert = part.querySelector().innerHTML;
+    type_dessert = part.querySelector(".item_title").innerHTML;
+
+    console.log(price_dessert);
 
     select_item();
 
 }
-
 
 
 /*========================
