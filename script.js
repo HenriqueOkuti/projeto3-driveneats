6c1d8c98c3927eacd5b99cfdaa205858647d93ea
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-10 21:10:14 -0300

    added index.html for github pages

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ let order_text;
     AUXILIAR FUNCTIONS
 ========================*/
 
-function pricenumber(price_string){
+function pricenumber(price_string) {
 
-    price_string = price_string.replace("R$ " , "");
-    price_string = price_string.replace("," , ".");
-    
+    price_string = price_string.replace("R$ ", "");
+    price_string = price_string.replace(",", ".");
+
     return Number(price_string);
 
 }
@@ -29,30 +29,30 @@ function pricenumber(price_string){
 
 function select_item() {
 
-            if (price_maindish !== undefined && price_drink !== undefined && price_dessert !== undefined){
+    if (price_maindish !== undefined && price_drink !== undefined && price_dessert !== undefined) {
 
-                const buttom_order = document.querySelector(".checkout a")
-                buttom_order.classList.add("active");
-                buttom_order.innerHTML = "Fechar pedido"
+        const buttom_order = document.querySelector(".checkout a")
+        buttom_order.classList.add("active");
+        buttom_order.innerHTML = "Fechar pedido"
 
-                const total = price_maindish + price_drink + price_drink;
-                order_text = `Olá, gostaria de fazer o pedido: \n- Prato: ${type_maindish} \n- Bebida: ${type_drink} \n- Sobremesa: ${type_dessert} \nTotal: R$ ${(total).toFixed(2)} `;
-                
+        const total = price_maindish + price_drink + price_drink;
+        order_text = `Olá, gostaria de fazer o pedido: \n- Prato: ${type_maindish} \n- Bebida: ${type_drink} \n- Sobremesa: ${type_dessert} \nTotal: R$ ${(total).toFixed(2)} `;
 
-                console.log(`Total: ${total.toFixed(2)}`);
-                console.log(order_text);
-                
-                console.log(type_maindish,type_drink,type_dessert);
-        
-            }
-        }
+
+        console.log(`Total: ${total.toFixed(2)}`);
+        console.log(order_text);
+
+        console.log(type_maindish, type_drink, type_dessert);
+
+    }
+}
 
 
 /*========================
     ROWS
 ========================*/
 
-function select_maindish(part){
+function select_maindish(part) {
     const selected_item = document.querySelector(".maindish .selected");
 
     if (selected_item !== null) {
@@ -71,7 +71,7 @@ function select_maindish(part){
 
 }
 
-function select_drink(part){
+function select_drink(part) {
     const selected_item = document.querySelector(".drink .selected");
 
     if (selected_item !== null) {
@@ -90,7 +90,7 @@ function select_drink(part){
 
 }
 
-function select_dessert(part){
+function select_dessert(part) {
     const selected_item = document.querySelector(".dessert .selected");
 
     if (selected_item !== null) {
@@ -117,7 +117,7 @@ function select_dessert(part){
 function finish_order() {
 
     const whatsapp = `https://wa.me/5516982266324?text=${encodeURIComponent(order_text)}`;
-    
+
     window.open(whatsapp);
 
     console.log(encodeURIComponent(order_text));
